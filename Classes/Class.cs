962957fe1119d46a9218b4a2e7539ffd6ba17ea8
Kol <62248045+Kol-Dev@users.noreply.github.com>
2022-04-12 12:16:48 +0200

    Create Class.cs

---
+++ Classes/Class.cs
@@ -0,0 +1,11 @@
+using System;
+namespace ConsoleProject.Classes
+{
+    public class Class
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+        public int Number { get; set; }
+        public ConsoleColor Color { get; set; }
+    }
+}
